2337a105e6ed9201e78e247828cadce0ce69f41b
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-07-30 16:32:28 +0200

    add block component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="flex flex-col items-center my-10">
+    <h1>Reaction timer</h1>
+    <button
+      :disabled="isPlaying"
+      @click="start"
+      class="bg-red-400 p-4 disabled:bg-red-200"
+    >
+      Play
+    </button>
+    <BlockComponent v-if="isPlaying" :delay="delay" />
+  </div>
 </template>
 
 <script>
+import BlockComponent from "./components/BlockComponent.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    BlockComponent,
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 500;
+      this.isPlaying = true;
+    },
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
 };
 </script>
-
-<style>
-</style>
---
+++ src/components/BlockComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    v-if="showBlock"
+    class="
+      w-96
+      rounded-2xl
+      bg-green-400
+      text-white text-center
+      px-0
+      py-24
+      mx-auto
+      my-10
+      cursor-pointer
+    "
+  >
+    <p>Click me</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  }
+};
+</script>
